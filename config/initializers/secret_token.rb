69ce246c84aef72abee6c7ecb6526581bdea92ad
Michael Morrissey <mmorrissey@market76.com>
2014-09-13 16:59:37 -0400

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lunar::Application.config.secret_token = 'cd531534d8003aa22e397451530892138a186851db7d0bcfcddee5f01c39d806ecdc1d065ec44c1cac4dc0e36060b7ba49eb50b30c7e5ca89dd0e40453b9561f'
