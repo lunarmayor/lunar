9d37d6e752e86fb2da49c430685e10fc667a1ef6
Michael Morrissey <mmorrissey@market76.com>
2014-11-07 19:51:11 -0500

    add web route

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Lunar::Application.routes.draw do
   root to: "application#index"
   get '/images', to: "application#index"
   get '/videos', to: "application#index"
+  get '/web', to: "application#index"
 end
